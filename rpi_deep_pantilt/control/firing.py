5d63908092179d986bb5dc06fae42929d5d11bdb
Maciej Kwiek <maciej.iai@gmail.com>
2023-05-23 21:52:14 +0200

    Make fire control use GPIO

--- rpi_deep_pantilt/control/firing.py
+++ rpi_deep_pantilt/control/firing.py
@@ -1,11 +1,48 @@
 import logging
+import RPi.GPIO as GPIO
+import time
 
 logging.basicConfig()
 LOGLEVEL = logging.getLogger().getEffectiveLevel()
 
+CHANNEL = 11
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(CHANNEL, GPIO.OUT, initial=GPIO.LOW)
+
+MIN_SWITCH_TIME = 1.0 
+
 class FireController:
+    lastFiredSwitchTime = time.perf_counter()
+    firing = False
+
     def Fire(self):
-        logging.warning("pew")
+        if self.firing:
+            return
+
+        currentTime = time.perf_counter()
+
+        logging.warning(currentTime - self.lastFiredSwitchTime)
+        if currentTime - self.lastFiredSwitchTime > MIN_SWITCH_TIME:
+            logging.warning("pew")
+            GPIO.output(CHANNEL, GPIO.HIGH)
+            self.firing = True
+            self.lastFiredSwitchTime = currentTime
 
     def ReadyToFire(self):
-        logging.warning("arming")
+        pass
+        #logging.warning("arming")
+
+    def Disengage(self):
+        if not self.firing:
+            return
+
+        currentTime = time.perf_counter()
+
+        logging.warning(currentTime - self.lastFiredSwitchTime)
+        if currentTime - self.lastFiredSwitchTime > MIN_SWITCH_TIME:
+            GPIO.output(CHANNEL, GPIO.LOW)
+            self.firing = False
+            self.lastFiredSwitchTime = currentTime
+
+    def Unacquire(self):
+        pass
